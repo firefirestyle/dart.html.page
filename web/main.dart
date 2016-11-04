68609d6eafee4adb5474153858d5fa385dfc4ab0
kyorohiro <kyorohiro@gmail.com>
2016-11-04 14:38:04 +0900

    ..

--- web/main.dart
+++ web/main.dart
@@ -1,14 +1,6 @@
 library firestylesite;
 
-import 'dart:html' as html;
-import 'dart:async';
 
-import 'package:firefirestyle.location/location.dart' as loc;
-import 'package:firefirestyle.location/location_html.dart' as loc;
-import 'package:crypto/crypto.dart' as crypto;
-import 'dart:convert' as conv;
-import 'dart:typed_data' as typed;
-import 'dart:math' as math;
 //
 import 'package:firefirestyle.html.page/page.dart' as page;
 
